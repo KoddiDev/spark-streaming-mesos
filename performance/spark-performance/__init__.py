788e768eb6e20b7cc0cc086898f5585efd93c09d
ArtRand <arand@soe.ucsc.edu>
2017-03-30 11:09:10 -0700

    moving things around, making wrapper objects for running jobs

--- performance/spark-performance/__init__.py
+++ performance/spark-performance/__init__.py
@@ -1,1 +1,36 @@
+import logging
 
+import shakedown
+import dcos.package
+
+# constants
+STRICT_MODE_ARGS = [
+    "--conf",
+    "spark.mesos.driverEnv.MESOS_MODULES=file:///opt/mesosphere/etc/mesos-scheduler-modules/dcos_authenticatee_module.json",
+    "--conf",
+    "spark.mesos.driverEnv.MESOS_AUTHENTICATEE=com_mesosphere_dcos_ClassicRPCAuthenticatee",
+    "--conf",
+    "spark.mesos.principal=service-acct"]
+
+DCOS_SPARK_RUN = "dcos --log-level=DEBUG spark --verbose run --submit-args=\"{spark_submit}\""
+
+
+# functions
+def requirePackage(pkg_name, options, install_required, LOGGER=None):
+    if LOGGER is None:
+        LOGGER = logging.getLogger(__name__)
+
+    pkg_manager = dcos.package.get_package_manager()
+    installed_pkgs = dcos.package.installed_packages(pkg_manager, None, None, False)
+
+    if any(pkg['name'] == pkg_name for pkg in installed_pkgs):
+        LOGGER.info("Package {} already installed.".format(pkg_name))
+        return True
+    else:
+        LOGGER.info("Installing package {}".format(pkg_name))
+        if install_required:
+            try:
+                return shakedown.install_package(pkg_name, options_json=options, wait_for_completion=True)
+            except Exception:
+                return False
+        return False
--- performance/spark-performance/sparkJob.py
+++ performance/spark-performance/sparkJob.py
@@ -1,6 +1,7 @@
 import os
 
-import s3
+from sparkperformance import requirePackage, STRICT_MODE_ARGS, DCOS_SPARK_RUN
+import sparkperformace.s3 as s3
 
 
 class AbstractSparkApplication(object):
@@ -14,33 +15,49 @@ class AbstractSparkApplication(object):
         self.app_url  = app_url
         self.app_args = applicaion_args
         self.strict   = strict
+        self.upload   = upload
+        self.uploaded = False
 
-    def run():
+    def getCmd():
         raise NotImplementedError
 
-    @staticmethod
-    def uploadApplicationFile(file_path):
+    def uploadApplicationFile(self, file_path):
         if not os.path.exists(file_path):
             return None, False
         s3.upload_file(file_path)
+        self.uploaded = True
         return s3.http_url(os.path.basename(file_path))
 
     def requirePackages(required_pkgs):
-
+        # type (List<(string, dict)>) where the dict is the associated options with the package
+        # defined by the string
+        for package, options in required_pkgs:
+            ok = requirePackage(package, options)
+            if not ok:
+                raise RuntimeError("Error installing required package" % package)
 
 
 class ScalaSparkApplication(AbstractSparkApplication):
     def __init__(self, app_url, applicaion_args, strict=False, upload=True):
-        super(AbstractSparkApplication)
-        pass
+        super(AbstractSparkApplication, self).__init__(app_url, applicaion_args, strict, upload)
+
+    def getCmd(self, required_packages, driver_mem):  # TODO add more spark configure options or abstract them
+        if not self.uploaded:
+            self.uploadApplicationFile(self.app_url)
+        self.requirePackages(required_packages)
+        spark_config_args = ["--conf", "spark.driver.memory=2g"]
+        if self.strict:
+            spark_config_args += STRICT_MODE_ARGS
+
+        spark_submit_args = " ".join(spark_config_args, self.app_url, self.app_args)
+        return DCOS_SPARK_RUN.format(spark_submit=spark_submit_args)
 
 
 class PythonSparkApplication(AbstractSparkApplication):
     def __init__(self, ):
-        pass
+        raise NotImplementedError
 
 
 class RSparkApplication(AbstractSparkApplication):
     def __init__(self, ):
-        pass
-
+        raise NotImplementedError
