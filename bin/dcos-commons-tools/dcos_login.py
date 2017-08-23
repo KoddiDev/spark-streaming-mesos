ddd4da9dd5b1835c1f84db4bcfca0a715707973a
ArtRand <arand@soe.ucsc.edu>
2017-08-23 16:15:52 -0700

    unbreak dcos_login.py

--- bin/dcos-commons-tools/dcos_login.py
+++ bin/dcos-commons-tools/dcos_login.py
@@ -15,72 +15,72 @@ log = logging.getLogger(__name__)
 
 
 def login(dcosurl: str, username: str, password: str, is_enterprise: bool) -> str:
-if is_enterprise:
-    log.info('logging into {} as {}'.format(dcosurl, username))
-    payload = {'uid': username, 'password': password}
-else:
-    log.info('logging into {} with default open token'.format(dcosurl))
-    payload = {'token': __CLI_LOGIN_OPEN_TOKEN}
+    if is_enterprise:
+        log.info('logging into {} as {}'.format(dcosurl, username))
+        payload = {'uid': username, 'password': password}
+    else:
+        log.info('logging into {} with default open token'.format(dcosurl))
+        payload = {'token': __CLI_LOGIN_OPEN_TOKEN}
 
-headers = {'Content-Type': 'application/json'}
-login_endpoint = '{dcosurl}/acs/api/v1/auth/login'.format(dcosurl=dcosurl)
-r = requests.post(login_endpoint, headers=headers, json=payload, verify=False)
-assert r.status_code == 200, '{} failed {}: {}'.format(login_endpoint, r.status_code, r.text)
-log.info('Login was successful!')
+    headers = {'Content-Type': 'application/json'}
+    login_endpoint = '{dcosurl}/acs/api/v1/auth/login'.format(dcosurl=dcosurl)
+    r = requests.post(login_endpoint, headers=headers, json=payload, verify=False)
+    assert r.status_code == 200, '{} failed {}: {}'.format(login_endpoint, r.status_code, r.text)
+    log.info('Login was successful!')
 
-return r.json()['token']
+    return r.json()['token']
 
 
 def _netloc(url: str):
-return url.split('-1')[-1]
+    return url.split('-1')[-1]
 
 
 def configure_cli(dcosurl: str, token: str) -> None:
-for cluster in dcos.cluster.get_clusters():
-    # check to see if the target cluster has been configured
-    if _netloc(cluster.get_url()) == _netloc(dcosurl):
-        dcos.cluster.set_attached(cluster.cluster_path)
-        log.info('Attached to already setup cluster: ' + cluster.cluster_id)
-        # cluster attached successfully, can begin using CLI/tests
-        return
-log.warning('Target cluster has not been setup yet. Performing setup...')
-with dcos.cluster.setup_directory() as temp_path:
-    dcos.cluster.set_attached(temp_path)
-    dcos.config.set_val('core.dcos_url', dcosurl)
-    dcos.config.set_val('core.ssl_verify', 'False')
-    dcos.config.set_val('core.dcos_acs_token', token)
-    dcos.cluster.setup_cluster_config(dcosurl, temp_path, False)
+    for cluster in dcos.cluster.get_clusters():
+        # check to see if the target cluster has been configured
+        if _netloc(cluster.get_url()) == _netloc(dcosurl):
+            dcos.cluster.set_attached(cluster.cluster_path)
+            log.info('Attached to already setup cluster: ' + cluster.cluster_id)
+            # cluster attached successfully, can begin using CLI/tests
+            return
+    log.warning('Target cluster has not been setup yet. Performing setup...')
+    with dcos.cluster.setup_directory() as temp_path:
+        dcos.cluster.set_attached(temp_path)
+        dcos.config.set_val('core.dcos_url', dcosurl)
+        dcos.config.set_val('core.ssl_verify', 'False')
+        dcos.config.set_val('core.dcos_acs_token', token)
+        dcos.cluster.setup_cluster_config(dcosurl, temp_path, False)
 
 
 def logout(dcosurl: str):
-pass
+    pass
 
 
 def login_session() -> None:
-"""Login to DC/OS.
+    """Login to DC/OS.
 
-Behavior is determined by the following environment variables:
-CLUSTER_URL: full URL to the test cluster
-DCOS_LOGIN_USERNAME: the EE user (defaults to bootstrapuser)
-DCOS_LOGIN_PASSWORD: the EE password (defaults to deleteme)
-DCOS_ENTERPRISE: determine how to authenticate (defaults to false)
-DCOS_ACS_TOKEN: bypass auth and use the user supplied token
-"""
-cluster_url = os.environ.get('CLUSTER_URL')
-if not cluster_url:
-    raise Exception('Must have CLUSTER_URL set in environment!')
-dcos_login_username = os.environ.get('DCOS_LOGIN_USERNAME', __CLI_LOGIN_EE_USERNAME)
-dcos_login_password = os.environ.get('DCOS_LOGIN_PASSWORD', __CLI_LOGIN_EE_PASSWORD)
-dcos_enterprise = os.environ.get('DCOS_ENTERPRISE', 'true').lower() == 'true'
-dcos_acs_token = os.environ.get('DCOS_ACS_TOKEN')
-if not dcos_acs_token:
-    log.info('No ACS token provided, logging in...')
-    dcos_acs_token = login(
-        dcosurl=cluster_url,
-        username=dcos_login_username,
-        password=dcos_login_password,
-        is_enterprise=dcos_enterprise)
-configure_cli(dcosurl=cluster_url, token=dcos_acs_token)
+    Behavior is determined by the following environment variables:
+    CLUSTER_URL: full URL to the test cluster
+    DCOS_LOGIN_USERNAME: the EE user (defaults to bootstrapuser)
+    DCOS_LOGIN_PASSWORD: the EE password (defaults to deleteme)
+    DCOS_ENTERPRISE: determine how to authenticate (defaults to false)
+    DCOS_ACS_TOKEN: bypass auth and use the user supplied token
+    """
+    cluster_url = os.environ.get('CLUSTER_URL')
+    if not cluster_url:
+        raise Exception('Must have CLUSTER_URL set in environment!')
+    dcos_login_username = os.environ.get('DCOS_LOGIN_USERNAME', __CLI_LOGIN_EE_USERNAME)
+    dcos_login_password = os.environ.get('DCOS_LOGIN_PASSWORD', __CLI_LOGIN_EE_PASSWORD)
+    dcos_enterprise = os.environ.get('DCOS_ENTERPRISE', 'true').lower() == 'true'
+    dcos_acs_token = os.environ.get('DCOS_ACS_TOKEN')
+    if not dcos_acs_token:
+        log.info('No ACS token provided, logging in...')
+        dcos_acs_token = login(
+            dcosurl=cluster_url,
+            username=dcos_login_username,
+            password=dcos_login_password,
+            is_enterprise=dcos_enterprise)
+    configure_cli(dcosurl=cluster_url, token=dcos_acs_token)
 
 
 if __name__ == '__main__':
